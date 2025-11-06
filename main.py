74546a5c3b37c39d3bd7b20c58557a4988898349
Daniel Zhu <danielfangzhu@gmail.com>
2025-11-06 10:43:37 -0800

    yummy
    Co-authored-by: monkeymagix123 <monkeymagix123@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
 from rv import RV
 from scipy import stats
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import numpy as np
 
-normal = RV.from_rv(stats.norm(loc=0, scale=1))
-tr = RV.sin(normal)
-tr.display()
-flr = RV.floor(normal)
-flr.display()
+uf = RV.uniform(0, 1)
+# uf.display("Uniform")
 
-x = normal + RV.from_constant(5)
-x.display()
+# Example 1: Inverse cdf method to do exponential
+exp = uf.apply(lambda a: -1 / 7 * np.log(1 - a))
+exp.display("Exponential")
+
+print("Exponential EV: ", exp.ev())
+print("Exponential var: ", exp.var())
+
+# Example 2: Inverse cdf method to do geometric RV
+p = 0.25
+geo = uf.apply(lambda a: np.ceil(np.log(1 - a) / np.log(1 - p)))
+geo.display("Geometric")
+
+print("Geometric EV: ", geo.ev())
+print("Geometric var: ", geo.var())
+
+# Example 3: Box-Muller transform
+u1 = RV.uniform(low=0, high=1)
+u2 = RV.uniform(low=0, high=1)
+
+z1 = RV.sqrt(RV.log(u1) * -2) * RV.cos(u2 * (2 * np.pi))
+z2 = RV.sqrt(RV.log(u1) * -2) * RV.sin(u2 * (2 * np.pi))
+
+z1.display("Box-Muller 1")
+z2.display("Box-Muller 2")
+
+# Example 4: Central Limit Theorem (CLT)
+def clt(cur, name):
+    for i in range(10):
+        cur = cur + cur
+    cur.display("CLT with 1024 " + name)
+
+clt(RV.uniform(0, 1), "uniform(0, 1)")
+clt(exp, "exponential")
+clt(RV.from_rv(stats.bernoulli(0.5)), "bernoulli")
+clt(RV.from_rv(stats.poisson(0.5)), "poisson")
+clt(RV.from_rv(stats.binom(n=10, p=0.2)), "binomial")
+
+# Example 5: Rejection Sampling Method
+# Take bounding box (assuming distribution finite support -- finite range), throw darts, see if land
+f = lambda x: 2 * x + 0.5 * np.sin(4 * np.pi * x) # PDF, between -0.5 and 2.5
+pdf = {
+    'func': f,
+    'range': (0, 1),
+    'max': 2.5
+}
+rej = RV.from_pdf(pdf)
+rej.display("Rejection Sampling")
 
 # Show plot
 plt.show()
--- rv.py
+++ rv.py
@@ -1,19 +1,25 @@
 import numpy as np
+from numbers import Number
 from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 class RV:
-    def __init__(self, rv=None, fn=None, lhs=None, rhs=None, cval=None):
+    def __init__(self, rv=None, fn=None, lhs=None, rhs=None, cval=None, pdf=None):
         self.rv = rv
         self.fn = fn
         self.lhs = lhs
         self.rhs = rhs
         self.cval = cval
+        self.pdf = pdf
 
     @classmethod
     def from_rv(cls, rv):
         return cls(rv=rv)
+
+    @classmethod
+    def uniform(cls, low, high):
+        return cls(rv=stats.uniform(loc=low, scale=high - low))
 
     @classmethod
     def from_operation(cls, fn, lhs, rhs):
@@ -23,7 +29,26 @@ class RV:
     def from_constant(cls, value):
         return cls(fn=None, lhs=None, rhs=None, cval=value)
 
-    def sample(self, samples=10000):
+    @classmethod
+    def from_pdf(cls, pdf):
+        return cls(pdf=pdf)
+
+    def sample(self, samples=100000):
+        if self.pdf is not None:
+            # Rejection sampling
+            x_min, x_max = self.pdf['range']
+            y_max = self.pdf['max']
+            samples_collected = []
+            while len(samples_collected) < samples:
+                x_samples = np.random.uniform(x_min, x_max, samples)
+                y_samples = np.random.uniform(0, y_max, samples)
+                for x, y in zip(x_samples, y_samples):
+                    if y <= self.pdf['func'](x):
+                        samples_collected.append(x)
+                    if len(samples_collected) >= samples:
+                        break
+            return np.array(samples_collected[:samples])
+
         if (self.cval is None) == False:
             return np.full(samples, self.cval)
 
@@ -36,28 +61,46 @@ class RV:
                 return self.fn(left_samples, right_samples)
 
         return self.rv.rvs(samples)
+    
+    def ev(self):
+        return np.mean(self.sample())
+    
+    def var(self):
+        return np.var(self.sample())
 
-    def display(self):
+    def display(self, title="RV Distribution"):
         plt.figure(figsize=(10, 6))
         # Plot histogram
-        plt.hist(self.sample(), bins=100, edgecolor='black', density=True)  # 'bins' controls number of bars
-        plt.title("Histogram of Data")
+        plt.hist(self.sample(), bins=200, edgecolor='black', density=True)  # 'bins' controls number of bars
+        plt.title(title)
         plt.xlabel("Value")
         plt.ylabel("Density")
         # sns.kdeplot(self.sample(), color='red', linestyle='--', linewidth=2, label='KDE (Smooth)')
 
     def __add__(self, other) -> 'RV':
+        if isinstance(other, Number):
+            return self.apply(lambda a: a + other)
+
         return RV.from_operation(lambda a, b: a + b, self, other)
 
     def __sub__(self, other) -> 'RV':
+        if isinstance(other, Number):
+            return self.apply(lambda a: a - other)
         return RV.from_operation(lambda a, b: a - b, self, other)
 
     def __mul__(self, other) -> 'RV':
+        if isinstance(other, Number):
+            return self.apply(lambda a: a * other)
         return RV.from_operation(lambda a, b: a * b, self, other)
     
     def __div__(self, other) -> 'RV':
+        if isinstance(other, Number):
+            return self.apply(lambda a: a / other)
         return RV.from_operation(lambda a, b: a / b, self, other)
     
+    def apply(self, fn) -> 'RV':
+        return RV.from_operation(fn, self, None)
+
     @staticmethod
     def min(a: 'RV', b: 'RV') -> 'RV':
         return RV.from_operation(np.minimum, a, b)
@@ -85,4 +128,12 @@ class RV:
     @staticmethod
     def floor(a: 'RV') -> 'RV':
         return RV.from_operation(np.floor, a, None)
+
+    @staticmethod
+    def log(a: 'RV') -> 'RV':
+        return RV.from_operation(np.log, a, None)
+
+    @staticmethod
+    def sqrt(a: 'RV') -> 'RV':
+        return RV.from_operation(np.sqrt, a, None)
 
